f8111bf5f2a099f1329f8f6958b53af7149ead9e
Zane Shelley <zshelle@us.ibm.com>
2026-06-26 16:02:51 -0500

    attn: general cleanup of Event class
    Change-Id: Ifa367364a086df856187e7a795ee2e88090d4ef8
    Signed-off-by: Zane Shelley <zshelle@us.ibm.com>

--- attn/attn-event.hpp
+++ attn/attn-event.hpp
@@ -33,17 +33,20 @@ class Event
         PRI_SPECIAL = 0,
     };
 
-    /** @brief Default constructor. */
-    Event() = delete;
-
-    /** @brief Main constructor. */
-    Event(Priority_t i_priority, int (*i_handler)(Event*),
+    /**
+     * @brief Constructor from components.
+     * @param i_priority The event priority.
+     * @param i_callback A callback function if this event needs to be handled.
+     * @param i_target   The target associated with this event.
+     * @param i_config   The current attention handler config.
+     */
+    Event(Priority_t i_priority, int (*i_callback)(Event*),
           TARGETING::TargetPtr i_target, Config* i_config) :
-        iv_priority(i_priority), iv_handler(i_handler), iv_target(i_target),
+        iv_priority(i_priority), iv_callback(i_callback), iv_target(i_target),
         iv_config(i_config)
     {}
 
-    /** @brief Destructor */
+    /** @brief Destructor. */
     ~Event() = default;
 
     /** @brief Copy constructor. */
@@ -52,41 +55,50 @@ class Event
     /** @brief Assignment operator. */
     Event& operator=(const Event&) = default;
 
-    /** @brief Get attention priority */
+    /** @brief Priority accessor. */
     Priority_t getPriority() const
     {
         return iv_priority;
     }
 
-    /** @brief Get config object */
-    Config* getConfig() const
-    {
-        return iv_config;
-    }
-
-    /** @brief Call attention handler function */
-    int handle()
-    {
-        return iv_handler(this);
-    }
-
-    /** @brief Get attention handler target */
+    /** @brief Target accessor. */
     TARGETING::TargetPtr getTarget() const
     {
         return iv_target;
     }
 
-    /** @brief less than operator, for heap creation */
+    /** @brief Config accessor. */
+    Config* getConfig() const
+    {
+        return iv_config;
+    }
+
+    /** @brief Initiates the callback function for this attention event. */
+    int handle()
+    {
+        return iv_callback(this);
+    }
+
+    /**
+     * @brief Less than operator. Used to sort these objects by event priority.
+     */
     bool operator<(const Event& right) const
     {
         return (getPriority() < right.getPriority());
     }
 
   private:
-    Priority_t iv_priority;         // The event priority.
-    int (*iv_handler)(Event*);      // handler function
-    TARGETING::TargetPtr iv_target; // handler function target
-    Config* iv_config;              // configuration flags
+    /** The event priority. */
+    Priority_t iv_priority;
+
+    /** A callback function if this event needs to be handled */
+    int (*iv_callback)(Event*);
+
+    /** The target associated with this event. */
+    TARGETING::TargetPtr iv_target;
+
+    /** The target associated with this event. */
+    Config* iv_config;
 };
 
 } // namespace attn
